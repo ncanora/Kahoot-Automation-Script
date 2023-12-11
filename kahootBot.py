df0e9e4aedbcf2c1f0835862150dc73d4dedf37e
Nick Canora <njc2@williams.edu>
2023-12-11 13:43:51 -0500

    Reverted changes back to using dict to store questions/answers Added support for questions that have multiple answers kahootAnswersLookUp now checks if the answer is mapped to the question

--- kahootBot.py
+++ kahootBot.py
@@ -1,6 +1,5 @@
 import os
 import time
-import re
 
 try:
     import selenium
@@ -15,56 +14,61 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
-### REAL STUFF ####
+### CODE ####
 
 def automateKahootClient(pin, name, answerTable):
-    service = Service(executable_path="chromedriver.exe", options=webdriver.ChromeOptions())
+    """
+    Takes in a pin, name, and answer table and joins the kahoot game and automatically answers questions
+    """
+    print("Joining Kahoot game...")
+    service = Service(executable_path="chromedriver.exe")
     driver = webdriver.Chrome(service=service)
 
     driver.get(f"https://kahoot.it/?pin={pin}&refer_method=link")
 
     time.sleep(2)
     WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.XPATH, '//input[@data-functional-selector="username-input"]'))
+        EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div/div/div/div[3]/div[2]/main/div/form/input"))
     )
-    nameBox = driver.find_element(By.XPATH, '//input[@data-functional-selector="username-input"]')
+    nameBox = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div/div/div[3]/div[2]/main/div/form/input")
     nameBox.send_keys(name + Keys.ENTER)
-
+    print(f"Successfully joined game {pin} as {name}!")
     ### ANSWERING ###
 
-
-
-    # Loop for next questions
+    # Loop for every question
     while True:
         try:
-
-             # Wait for any question element to appear
+            
+            # Wait for the question element to appear
             question_locator_wait = WebDriverWait(driver, 300)
-            question_locator_wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="shadow-text__Text-sc-o7uc06-1 cAcSib" and contains(text(), "Question")]')))
-
-            # Find question element
-            question_element = driver.find_element(By.XPATH, '//div[@class="shadow-text__Text-sc-o7uc06-1 cAcSib" and contains(text(), "Question")]')
+        
+            question_locator_wait.until(EC.presence_of_element_located((By.XPATH, f'//div[@class="shadow-text__Text-sc-o7uc06-1 cAcSib" and contains(text(), "Question")]')))
+            question_element = driver.find_element(By.XPATH, f'//div[@class="shadow-text__Text-sc-o7uc06-1 cAcSib" and contains(text(), "Question")]')
 
             # Extract the question number from the element text
             question_number_text = question_element.text
             question_number = int(question_number_text.replace("Question", "").strip())
 
-            print(f"Question {question_number}: {answerTable[(int(len(answerTable)/2)) + question_number]}")
+            print(f"Question {question_number}: Number {question_number} also {answerTable.get(question_number)}")
 
             # Answer the question
-            kahootClientAnswer(driver, int(answerTable[(int(len(answerTable)/2) + question_number)-1][7]))
-            question_number += 1
+            kahootClientAnswer(driver, int(answerTable.get(question_number)[7]))
+            time.sleep(.5)
 
         except Exception as e:
             # Program waits max of 5 mins for questions to appear before terminating
             print(e)
-            print("program broke rip")
+            print("Something has gone wrong or the program has terminated")
+            time.sleep(5)
             break
     
     driver.quit()
 
     
 def kahootClientAnswer(driver, option):
+    """
+    Takes in driver and an option number (1-4) and clicks the corresponding answer button
+    """
     if option == 1:
         buttonWait = WebDriverWait(driver, 300)
         buttonWait.until(
@@ -99,50 +103,51 @@ def kahootClientAnswer(driver, option):
 
 def kahootAnswersLookUp(quizid):
     """
-    Takes in a quizID (in the url of a kahoot quiz) and returns a dictionary of questions mapped to correct answers
+    Takes in driver and a quizID (in the url of a kahoot quiz) and returns a dictionary of questions mapped to a single correct answer (even if their are multiple correct answers for that question)
     """
-    
+    print("Building answer table... please wait")
     service = Service(executable_path="chromedriver.exe")
     driver = webdriver.Chrome(service=service)
 
-    
+    # Wait for web elements to load
     driver.get("https://create.kahoot.it/details/" + quizid)
     waitForButton = WebDriverWait(driver, 10)
     waitForButton.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div/main/div[2]/div[2]/div[2]/div/section[1]/div/button")))
     driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/main/div[2]/div[2]/div[2]/div/section[1]/div/button").click()
-
-    questions_answers = []
     time.sleep(1)
     
-    # List of question and correct answer elements
+    answerTable = {}
+    questions = driver.find_elements(By.CLASS_NAME, "question-item__QuestionListItem-sc-1evx9zu-0")
+
+    # Loop through each question and find the correct answer(s)
+    for i, question in enumerate(questions, start=1):
+        question_text = question.find_element(By.CLASS_NAME, "styles__Question-sc-19vxqaz-6").text
+        answer_options = question.find_elements(By.CLASS_NAME, "styles__Choice-sc-19vxqaz-17")
+
+        # Loop through each answer child of the question
+        for option in answer_options:
+            answer_text = option.find_element(By.CLASS_NAME, "styles__Answer-sc-19vxqaz-20").text
+            aria_label = option.get_attribute("aria-label")
+
+            # Check if the answer is correct
+            if "incorrect" not in aria_label and "correct" in aria_label:
+                # Answers stored as "Option X - Answer Text"
+                answ = aria_label.split("-")
+                answerTable[i] = f'{answ[0]}-{answ[1]}'
     
-    question_elements = driver.find_elements(By.XPATH, '//span[@class="styles__Question-sc-19vxqaz-6 ejwdwI"]')
-    answer_elements = answer_elements = driver.find_elements(By.XPATH, '//div[contains(@class, "styles__Choice-sc-19vxqaz-17 illeca") and contains(@aria-label, "correct") and not(contains(@aria-label, "incorrect"))]')
-
-    # Loop through each question
-    # Uses zip() to make a tuple for each iteration
-    for index, (question_elementt) in enumerate(question_elements, start=1):
-        questions_answers.append(index)
-    
-
-    for index, (answer_element) in enumerate(answer_elements, start=1):
-        questions_answers.append(answer_element.get_attribute("aria-label"))
-
+    print("Build success!")
     driver.quit()
-    return questions_answers
+    return answerTable
     
 
 
 
 if __name__ == "__main__":
 
-    answerTable = []
-    quizID = input ("Please enter quizID (should be in the url of hosts): ")
+    quizID = input("Enter the quizID (should be in the url of hosts browser): ")
     quizCode = input("Enter the quiz code: ")
-    nickname = input ("Enter your nickname: ")
-    #randomInterval = input("Enter Y or N for random interval answering: ")
-    kahootAnswersLookUp(quizID)
+    nickname = input("Enter your desired nickname: ")
+    
+    print("Starting bot...")
+    answerTable = kahootAnswersLookUp(quizID)
     automateKahootClient(quizCode, nickname, answerTable)
-
-
-
